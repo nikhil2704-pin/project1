b176b4343a5624049f00ca3869eea6aa03e92200
nikhil2704-pin <nikhilpingle93@gmail.com>
2021-04-21 17:09:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(getApplicationContext(), login.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/example/project1/login.java
@@ -0,0 +1,43 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+    Button button;
+    EditText phnum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.button);
+        phnum = findViewById(R.id.phonenumber1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phonenumber = phnum.getText().toString();
+
+                String correctphonenumber = "123456789";
+                if(phonenumber.equalsIgnoreCase(correctphonenumber)) {
+                    Toast.makeText(getApplicationContext(),"YOU ARE LOGGED IN",Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), checkotp.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "LOGIN UNSUCCESFULL", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+}
